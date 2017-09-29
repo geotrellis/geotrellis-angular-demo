2b37a28c4aeca2b70d4e0349a501e2684a453f4a
Taylor Nation <tnation14@gmail.com>
2017-09-29 14:35:00 -0400

    Scripts/infra should deploy docs

--- scripts/infra.sh
+++ scripts/infra.sh
@@ -2,63 +2,53 @@
 
 set -e
 
-if [[ -n "${GEOTRELLIS_DEMOS_DEBUG}" ]]; then
+if [[ -n "${GEOTRELLIS_DEMO_DEBUG}" ]]; then
     set -x
 fi
 
-DIR="$(dirname "$0")"
-TERRAFORM_DIR="${DIR}/../deployment/terraform"
-
 function usage() {
     echo -n \
-"Usage: $(basename "$0") COMMAND OPTION[S]
+         "Usage: $(basename "$0") COMMAND [OPTIONS]
+
 Execute Terraform subcommands with remote state management.
+
+Required:
+
+  AWS_PROFILE: Configuration profile name used to create AWS resources
+  GEOTRELLIS_DEMO_SITE_BUCKET: Bucket to house the application; CDN origin
+  GEOTRELLIS_DEMO_SETTINGS_BUCKET: Bucket to house infrastructure state and variables
+
 "
 }
 
-if [ "${BASH_SOURCE[0]}" = "${0}" ]; then
-    if [ "${1:-}" = "--help" ]; then
+DIR=$(dirname ${0})
+if [ "${BASH_SOURCE[0]}" = "${0}" ]
+then
+    if [ "${1:-}" = "--help" ]
+    then
         usage
     else
-        if [[ -n "${GEOTRELLIS_DEMOS_SETTINGS_BUCKET}" ]]; then
-            TF_FILE_BASE_NAME=$(echo "${GEOTRELLIS_DEMOS_SETTINGS_BUCKET}" | tr . -)
+        if [[ -n "${GEOTRELLIS_DEMO_SETTINGS_BUCKET}" ]] && [[ -n "${GEOTRELLIS_DEMO_SITE_BUCKET}" ]]; then
 
-            pushd "${TERRAFORM_DIR}"
+            pushd "$(dirname "$0")/../deployment/terraform"
 
-            # Stop Terraform from trying to apply incorrect state across environments
-            if [ -f ".terraform/terraform.tfstate" ] && ! grep -q "${GEOTRELLIS_DEMOS_SETTINGS_BUCKET}" ".terraform/terraform.tfstate"; then
-                echo "ERROR: Incorrect target environment detected in Terraform state! Please run"
-                echo "       the following command before proceeding:"
-                echo
-                echo "  rm -rf deployment/terraform/.terraform"
-                echo
-                exit 1
-            fi
-
-            aws s3 cp "s3://${GEOTRELLIS_DEMOS_SETTINGS_BUCKET}/terraform/terraform.tfvars" "${TF_FILE_BASE_NAME}.tfvars"
-
-            terraform remote config \
-                      -backend="s3" \
-                      -backend-config="region=us-east-1" \
-                      -backend-config="bucket=${GEOTRELLIS_DEMOS_SETTINGS_BUCKET}" \
-                      -backend-config="key=terraform/state" \
-                      -backend-config="encrypt=true"
+            aws s3 cp "s3://${GEOTRELLIS_DEMO_SETTINGS_BUCKET}/terraform/terraform.tfvars" "${GEOTRELLIS_DEMO_SETTINGS_BUCKET}.tfvars"
 
             case "${1}" in
-                fmt)
-                    terraform "$@"
-                    ;;
-                taint)
-                    terraform "$@"
-                    ;;
                 plan)
+                    rm -rf .terraform terraform.tfstate*
+                    terraform init -backend-config="bucket=${GEOTRELLIS_DEMO_SETTINGS_BUCKET}" \
+                                   -backend-config="key=terraform/state"
+
                     terraform plan \
-                              -var-file="${TF_FILE_BASE_NAME}.tfvars" \
-                              -out="${TF_FILE_BASE_NAME}.tfplan"
+                              -var-file="${GEOTRELLIS_DEMO_SETTINGS_BUCKET}.tfvars" \
+                              -out="${GEOTRELLIS_DEMO_SETTINGS_BUCKET}.tfplan"
+
+                    aws s3 sync --dryrun --delete ../../dist/_site "s3://${GEOTRELLIS_DEMO_SITE_BUCKET}"
                     ;;
                 apply)
-                    terraform apply "${TF_FILE_BASE_NAME}.tfplan"
-                    terraform remote push
+                    terraform apply "${GEOTRELLIS_DEMO_SETTINGS_BUCKET}.tfplan"
+                    aws s3 sync --delete ../../dist/_site "s3://${GEOTRELLIS_DEMO_SITE_BUCKET}"
                     ;;
                 *)
                     echo "ERROR: I don't have support for that Terraform subcommand!"
@@ -68,7 +58,7 @@ if [ "${BASH_SOURCE[0]}" = "${0}" ]; then
 
             popd
         else
-            echo "ERROR: No GEOTRELLIS_DEMOS_SETTINGS_BUCKET variable defined."
+            echo "ERROR: No GEOTRELLIS_DEMO_SETTINGS_BUCKET or GEOTRELLIS_DEMO_SITE_BUCKET variable defined."
             exit 1
         fi
     fi
