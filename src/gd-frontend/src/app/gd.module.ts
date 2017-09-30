24be28948f33a54fa4f5741bab12f27407eb820a
YunS-Stacy <shiyun_stacy@yahoo.com>
2017-09-30 00:55:22 -0400

    refactor app into sub modules

--- src/gd-frontend/src/app/gd.module.ts
+++ src/gd-frontend/src/app/gd.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { HttpClientModule } from '@angular/common/http';
-
 import { GdComponent } from './gd.component';
 import { GdRoutingModule } from './gd-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    // HttpClientModule,
     GdRoutingModule,
   ],
   declarations: [
